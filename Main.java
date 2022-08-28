8bd887cf44dda7d19f8a1dd15ef159b49b00b4bd
Joker-124 <106297120+Joker-124@users.noreply.github.com>
2022-08-28 15:04:05 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,19 @@
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Your current Height: ");
-        int height = sc.nextInt();
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Your Fathers Height: ");
-        int father = sc.nextInt();
-        Scanner sc2 = new Scanner(System.in);
-        System.out.println("Your Mothers Height: ");
-        int mother = sc.nextInt();
-        int adult = (father+mother)/2;
-        System.out.println("Your previous height was "+height+"cm\nYour Adult height will be "+adult+"cm");
+            System.out.println("Your current Height: ");
+            int height = sc.nextInt();
+            Scanner sc1 = new Scanner(System.in);
+            System.out.println("Your Fathers Height: ");
+            int father = sc.nextInt();
+            Scanner sc2 = new Scanner(System.in);
+            System.out.println("Your Mothers Height: ");
+            int mother = sc.nextInt();
+            int adultboy = (int) ((father+mother)/2+2.5);
+            int adultgirl = (int) ((father+mother)/2-2.5);
+            System.out.println("Your Future Height as a Male would be "+adultboy+"cm");
+            System.out.println("Your Future Height as a Female would be "+adultgirl+"cm");
+        }
     }
-}
